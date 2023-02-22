bbc25d8ccf63ff943632d06c4416ea4d0c810eab
broyettore <broyettore@gmail.com>
2023-02-22 18:40:31 +0100

    main done next is linking form to js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,81 @@
 'use strict';
 
-// VARIABILI 
+// NAV NENU 
 
-// const utenteNome = prompt("Ciao, come si chiama ?");
-// const utenteKm = Number(prompt("Quanti km vuole percorrere ?"));
-// const utenteEtà = Number(prompt("Quanti anni ha ?"));
-const prezzoBase = 0.21 * utenteKm
-const scontoMinorenni = prezzoBase - (prezzoBase / 100) * 20
-const scontoAnziani = prezzoBase - (prezzoBase / 100) * 40
+const navMenu = document.querySelector(".nav-menu");
+const hamburgerIcon = document.querySelector(".hamburger");
 
+hamburgerIcon.addEventListener("click", showMenu);
 
-// CONDIZIONI DI SCONTO 
+function showMenu() {
 
-
-if (utenteEtà < 18) {
-    // document.querySelector(".price").innerHTML = `Il prezzo per questa distanza è di ${Number(scontoMinorenni.toFixed(2))}€`;
-} else if (utenteEtà > 65) {
-    // document.querySelector(".price").innerHTML = `Il prezzo per questa distanza è di ${Number(scontoAnziani.toFixed(2))}€`;
+    hamburgerIcon.classList.toggle("fa-xmark");
+    hamburgerIcon.classList.toggle("fa-bars");
+    navMenu.classList.toggle("show");
 } 
+
+// THEME SWITCH 
+
+const themeLightBtn = document.querySelector(".ms-sun");
+
+themeLightBtn.addEventListener("click", changeTheme);
+
+function changeTheme() {
+
+    themeLightBtn.classList.toggle("fa-moon");
+    themeLightBtn.classList.toggle("ms-sun");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // VARIABILI 
+
+// const nomeUtente = document.getElementById("name");
+// const kmUtente = document.getElementById("distance").value;
+// const etàUtente = document.getElementById("age");
+
+// const prezzoGenera = document.querySelector(".generate");
+// const userPrice = document.querySelector(".price");
+
+// const prezzoBase = 0.21 * kmUtente;
+// const scontoMinorenni = prezzoBase - (prezzoBase / 100) * 20
+// const scontoAnziani = prezzoBase - (prezzoBase / 100) * 40
+
+// // EVENTI 
+
+// prezzoGenera.addEventListener("click", showPrice)
+
+// function showPrice () {
+
+//     userPrice.innerHTML = prezzoBase
+
+//     // CONDIZIONI DI SCONTO 
+
+//     if (etàUtente.value == "user-age-small") {
+//         userPrice.innerHTML = Number(scontoMinorenni.toFixed(2));
+//     } else if (etàUtente.value == "user-age-old") {
+//         userPrice.innerHTML = Number(scontoAnziani.toFixed(2));
+//     }
+// }
+
+// kmUtente.addEventListener("change", updateKm)
+
+// function updateKm () {
+//     userPrice.innerHTML = prezzoBase
+// }
+
+
+
 
 
 
